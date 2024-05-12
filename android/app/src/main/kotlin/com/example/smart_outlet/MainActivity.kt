4ac43bd8ce1d7e681ea81e67593d8b3ce173ba10
razken-hash <ka_kenniche@esi.dz>
2024-05-12 08:58:54 +0100

    SmartOutlet Project Init

---
+++ android/app/src/main/kotlin/com/example/smart_outlet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smart_outlet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartOutletApp());
+}
+
+class SmartOutletApp extends StatelessWidget {
+  const SmartOutletApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
